6bf3c0df2962d4de72fa080486c6fd2955de95d6
Taras Kozodoi <acateely@gmail.com>
2023-09-25 18:04:44 +0300

    init express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log('Clinic api running.')
+import dotenv from 'dotenv'
+import express from 'express'
+
+//read .env file
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Express work correct' })
+})
+
+app.listen(port, () => {
+  console.log(`[SERVER]: Server is running at http://localhost:${port}`)
+})
