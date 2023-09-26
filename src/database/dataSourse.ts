8aede5243f6871c7b0cfd1ee3443b2703e487254
Taras Kozodoi <acateely@gmail.com>
2023-09-26 11:59:59 +0300

    fix dont find entities

--- src/database/dataSourse.ts
+++ src/database/dataSourse.ts
@@ -7,7 +7,7 @@ const dataSourse = new DataSource({
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
-  entities: ['./entity/*.ts'],
+  entities: [`${__dirname}/entity/*.ts`],
   logging: process.env.TYPEORM_LOGGING === 'true',
   synchronize: true, // delete for production
 })
