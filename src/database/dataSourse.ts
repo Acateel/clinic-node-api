02afdbb9c5df8fb91960a461c793fe5c2d616076
Taras Kozodoi <acateely@gmail.com>
2023-09-26 13:47:26 +0300

    add ssl true for database

--- src/database/dataSourse.ts
+++ src/database/dataSourse.ts
@@ -7,6 +7,7 @@ const dataSourse = new DataSource({
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
+  ssl: true,
   entities: [`${__dirname}/entity/*.ts`],
   logging: process.env.TYPEORM_LOGGING === 'true',
 })
