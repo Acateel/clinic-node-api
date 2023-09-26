f1f5a8e1f57e5e98cb0d5f26a9314dfa041fe530
Taras Kozodoi <acateely@gmail.com>
2023-09-26 12:00:20 +0300

    add parser for request body

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,21 @@ import express from 'express'
 import doctorRouter from './routers/doctor'
 import patientRouter from './routers/patient'
 import appointmentRouter from './routers/appointment'
+import bodyParser from 'body-parser'
 
 const app = express()
 
+// add body parser 
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
 // Include routers
 app.use('/doctors', doctorRouter)
-app.use('/patient', patientRouter)
-app.use('/appointment', appointmentRouter)
+app.use('/patients', patientRouter)
+app.use('/appointments', appointmentRouter)
 
 export default app
