50225b593bfb867c91928f4ef28ce6f681884174
Taras Kozodoi <acateely@gmail.com>
2023-10-02 20:29:30 +0300

    add util for format phone number

---
+++ src/util/format-phone-number.ts
@@ -0,0 +1,11 @@
+/**
+ * Convert phone number in format: (XXX) XXX-XXXX
+ */
+export const formatPhoneNumber = (phoneNumber: string) => {
+  var cleaned = phoneNumber.replace(/\D/g, '')
+  var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+  if (match) {
+    return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+  }
+  return null
+}
