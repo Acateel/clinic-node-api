00157bce549be612e8cc92408a4e66bf0450dc70
Taras Kozodoi <acateely@gmail.com>
2023-10-05 20:10:40 +0300

    add check new appointmet times for create

--- src/services/appointment-service.ts
+++ src/services/appointment-service.ts
@@ -1,5 +1,7 @@
+import { Between } from 'typeorm'
 import { dataSourse } from '../database/data-sourse'
 import { AppointmentEntity } from '../database/entity/appointment-entity'
+import createHttpError from 'http-errors'
 
 class AppointmentService {
   async get() {
@@ -17,13 +19,19 @@ class AppointmentService {
   async create(
     patientId: number,
     doctorId: number,
-    startTime: string,
-    endTime: string
+    startTime: Date,
+    endTime: Date
   ) {
     const appointmentRepo = dataSourse.getRepository(AppointmentEntity)
 
-    // will add what startTime bigger then previus appointment.endTime
-    // will add what endTime less then next appointment.startTime
+    const canAddNewAppointment = await this.checkAppoitmentTimes(
+      doctorId,
+      startTime,
+      endTime
+    )
+    if (!canAddNewAppointment) {
+      throw createHttpError(400, 'Cannot add appointment in this time')
+    }
 
     const appointment = appointmentRepo.create({
       patient: { id: patientId },
@@ -34,6 +42,57 @@ class AppointmentService {
     const result = await appointmentRepo.save(appointment)
 
     return result
+  }
+
+  async getByDoctorIdAndDay(doctorId: number, date: Date) {
+    const appointmentRepo = dataSourse.getRepository(AppointmentEntity)
+
+    const startOfDay = new Date(
+      `${date.toISOString().split('T')[0]}T00:00:00.000Z`
+    )
+    const endOfDay = new Date(
+      `${date.toISOString().split('T')[0]}T23:59:59.999Z`
+    )
+
+    const appointmentsByDay = await appointmentRepo.find({
+      where: {
+        doctor: {
+          id: doctorId,
+        },
+        startTime: Between(startOfDay, endOfDay),
+      },
+      order: {
+        startTime: 'ASC',
+      },
+    })
+
+    return appointmentsByDay
+  }
+
+  async checkAppoitmentTimes(doctorId: number, startTime: Date, endTime: Date) {
+    const appointmentsByDay = await this.getByDoctorIdAndDay(
+      doctorId,
+      startTime
+    )
+
+    if (
+      appointmentsByDay.length === 0 ||
+      endTime <= appointmentsByDay[0].startTime ||
+      startTime >= appointmentsByDay[appointmentsByDay.length - 1].endTime
+    ) {
+      return true
+    }
+
+    for (let i = 0; i < appointmentsByDay.length - 1; i++) {
+      if (
+        startTime >= appointmentsByDay[i].endTime &&
+        endTime <= appointmentsByDay[i + 1].startTime
+      ) {
+        return true
+      }
+    }
+
+    return false
   }
 
   async update() {}
