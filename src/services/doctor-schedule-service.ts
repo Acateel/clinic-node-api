d788b01aaa2c1687c6dbc5143a29db9c657cb4b3
Taras Kozodoi <acateely@gmail.com>
2023-10-25 21:44:25 +0300

    feat: add doctor schedule service

---
+++ src/services/doctor-schedule-service.ts
@@ -0,0 +1,54 @@
+import { dataSourse } from '../database/data-sourse'
+import { DoctorScheduleEntity } from '../database/entity/doctor-schedule-entity'
+
+class DoctorScheduleService {
+  async get() {
+    const scheduleRepo = dataSourse.getRepository(DoctorScheduleEntity)
+    const schedules = await scheduleRepo.find()
+    return schedules
+  }
+
+  async getByDoctorId(doctorId: number) {
+    const scheduleRepo = dataSourse.getRepository(DoctorScheduleEntity)
+    const schedules = await scheduleRepo.findBy({ doctor: { id: doctorId } })
+    return schedules
+  }
+
+  async getById(id: number) {
+    const scheduleRepo = dataSourse.getRepository(DoctorScheduleEntity)
+    const schedule = await scheduleRepo.findOneBy({ id })
+    return schedule
+  }
+
+  async create(doctorId: number, startTime: Date, endTime: Date) {
+    const scheduleRepo = dataSourse.getRepository(DoctorScheduleEntity)
+    const schedule = scheduleRepo.create({
+      doctor: { id: doctorId },
+      startTime,
+      endTime,
+    })
+    const result = await scheduleRepo.save(schedule)
+    return result
+  }
+
+  async update(id: number, doctorId: number, startTime: Date, endTime: Date) {
+    const scheduleRepo = dataSourse.getRepository(DoctorScheduleEntity)
+    const schedule = await scheduleRepo.findOneBy({ id })
+    scheduleRepo.merge(schedule, {
+      doctor: { id: doctorId },
+      startTime,
+      endTime,
+    })
+
+    const result = await scheduleRepo.save(schedule)
+    return result
+  }
+
+  async delete(id: number) {
+    const scheduleRepo = dataSourse.getRepository(DoctorScheduleEntity)
+    const result = await scheduleRepo.delete(id)
+    return result
+  }
+}
+
+export const doctorScheduleService = new DoctorScheduleService()
